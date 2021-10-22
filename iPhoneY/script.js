291147e51641fe6784c2fb9135b6eacc46167f96
BonnieToGamer <bonnietogamer@lemons.se>
2021-10-22 12:02:13 +0200

    22/10/21

--- iPhoneY/script.js
+++ iPhoneY/script.js
@@ -13,7 +13,30 @@ var gallImg1 = document.getElementById("gallImg1");
 var gallImg2 = document.getElementById("gallImg2");
 var gallImg3 = document.getElementById("gallImg3");
 
-function galleryRight() {
+function galleryRight(obj) {
+    obj.disabled = true;
+    image1++;
+    image2++;
+    image3++;
+    if (image1 > images.length - 1) {
+        image1 = 0;
+    }
+
+    if (image2 > images.length - 1) {
+        image2 = 0;
+    }
+
+    if (image3 > images.length - 1) {
+        image3 = 0;
+    }
+
+    changeImg();
+    obj.disabled = false;
+}
+
+function galleryLeft(obj) {
+    obj.disabled = true;
+
     image1--;
     image2--;
     image3--;
@@ -29,31 +52,13 @@ function galleryRight() {
     if (image3 < 0) {
         image3 = images.length - 1;
     }
-    changeImg();
-}
-
-function galleryLeft() {
-    image1++;
-    image2++;
-    image3++;
-    if (image1 > images.length) {
-        image1 = 0;
-    }
-
-    if (image2 > images.length) {
-        image2 = 0;
-    }
-
-    if (image3 > images.length) {
-        image3 = 0;
-    }
 
     changeImg();
+    obj.disabled = false;
 }
 
 function changeImg() {
     gallImg1.src = `images/${images[image1]}.jpg`;
     gallImg2.src = `images/${images[image2]}.jpg`;
     gallImg3.src = `images/${images[image3]}.jpg`;
-    console.log(`images/${images[image1]}.jpg`);
 }
