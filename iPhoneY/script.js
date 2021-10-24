85c37d31b627968752131a6adf9bf7023e44749c
BonnieToGamer <BonnieToGamer@lemons.se>
2021-10-24 16:30:08 +0200

    21/10/24

--- iPhoneY/script.js
+++ iPhoneY/script.js
@@ -4,6 +4,11 @@ var images = [
     "phone3", "phoneCamera"
 ];
 
+var captions = [
+    "All new 1mp lens", "New 1mp camera with horrible zoom", "3 Cameras for even better photos",
+    "A completley new display with vibrant colors", "Brand new matt black color", "Can not give the same quality as a DSLR camera"
+]
+
 // Default images
 var image1 = 2;
 var image2 = 3;
@@ -12,8 +17,9 @@ var image3 = 4;
 var gallImg1 = document.getElementById("gallImg1");
 var gallImg2 = document.getElementById("gallImg2");
 var gallImg3 = document.getElementById("gallImg3");
+var caption = document.getElementById("caption");
 
-function galleryLeft(obj) {
+function galleryRight(obj) {
     obj.disabled = true;
     image1++;
     image2++;
@@ -29,12 +35,11 @@ function galleryLeft(obj) {
     if (image3 > images.length - 1) {
         image3 = 0;
     }
-
     changeImg();
     obj.disabled = false;
 }
 
-function galleryRight(obj) {
+function galleryLeft(obj) {
     obj.disabled = true;
 
     image1--;
@@ -61,4 +66,5 @@ function changeImg() {
     gallImg1.src = `images/${images[image1]}.jpg`;
     gallImg2.src = `images/${images[image2]}.jpg`;
     gallImg3.src = `images/${images[image3]}.jpg`;
+    caption.innerHTML = captions[image2];
 }
